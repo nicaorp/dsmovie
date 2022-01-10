afc961ce9a909e284c1c56daf7744710cd49d629
Nicolas Ferreira <nicolasferreira100@gmail.com>
2022-01-10 16:36:16 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,36 @@
 import Pagination from "../../components/Pagination";
+import MovieCard from "../../components/MovieCard";
 
 function Listing() {
     return (
-    <Pagination />
+        <>
+            <Pagination />,
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mb-3">   
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+
+        </>
     );
 }
 
