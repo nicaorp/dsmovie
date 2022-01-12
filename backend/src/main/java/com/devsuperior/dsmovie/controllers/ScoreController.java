da22d000e9062fabe04fdcd1fdc912e415a4ad57
Nicolas Ferreira <nicolasferreira100@gmail.com>
2022-01-12 13:15:21 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -20,7 +20,7 @@ public class ScoreController {
 	@PutMapping
 	 public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
 		MovieDTO movieDTO = service.saveScore(dto);
-		return movieDTO;
+		return movieDTO;	 
 		
 	}
 	
